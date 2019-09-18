a6d9e7fe61d67a70b03f4a471eb325d6b117845c
James Grams <james@game103.net>
2019-09-18 14:31:59 -0400

    Updated connect to work better with wii

--- helper/controller_connect.sh
+++ helper/controller_connect.sh
@@ -4,6 +4,7 @@ do
 	PAIRED=$(bash -c "echo -e 'paired-devices\nquit' | bluetoothctl" | grep '^Device' | awk '{print $2}')
 	while read -r line; do
 		bash -c "echo -e 'connect $line\nquit' | bluetoothctl"
+		sleep 5
 	done <<< $PAIRED
 	sleep 5
 done
