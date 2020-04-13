60a2cd7e11e746b2b3abc7dc7c9141dc22e161f9
James Grams <james.grams@gordon.edu>
2020-04-13 09:39:23 -0400

    Bug fix

--- src/index.js
+++ src/index.js
@@ -2747,7 +2747,7 @@ function updateGame( oldSystem, oldGame, oldParents=[], system, game, file, pare
 
     // After dirs are done, we can run getData
     // Update the data (this will take care of making all the necessary directories for the game as well as updating the data)
-    if( typeof file != STRING_TYPE ) runWhenDone();
+    if( !file || typeof file != STRING_TYPE ) runWhenDone();
     else getData(); // Run get data now, so we can have an updated gamesDict even if we haven't finished downloading yet
     resolveDirsDone();
 
