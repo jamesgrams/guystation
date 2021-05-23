85ce7f0a47d6ade754aaf0f3689dd8ddbcb6efdb
James Grams <james@game103.net>
2021-05-23 09:33:16 -0400

    Bug fix

--- src/index.js
+++ src/index.js
@@ -2398,7 +2398,7 @@ async function getData( startup, noPlaying, nonessential, forceStream ) {
                     systemData.games = JSON.parse(await fsExtra.readFile(STREAM_INFO_PATH)).dict;
                 }
                 catch(err) {
-                    systemsData.games = {};
+                    systemData.games = {};
                 }
             }
             newSystemsDict[system] = systemData;
