9de5256e5ea22b1a00e73d2653bbee52d1a09bc1
James Grams <james@game103.net>
2021-06-18 15:59:31 -0400

    Match chrome correctly

--- src/index.js
+++ src/index.js
@@ -273,7 +273,7 @@ const SET_MUTE_COMMAND = "pacmd set-sink-input-mute ";
 const MUTE_TRUE = " true";
 const MUTE_FALSE = " false";
 const BLANK_PAGE = "about:blank";
-const CHROME_NAME = "application.name = \"Chrom";
+const CHROME_NAME = /application\.name = \"(Google )?Chrom/;
 const MUTE_MODES = {
     none: "none",
     game: "game",
