dee0fadc8dfeadeec77696b5523ceadbff9ea022
James Grams <james@game103.net>
2022-02-15 15:50:31 -0500

    Stealth

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@
  */
 
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 const proc = require( 'child_process' );
 const fs = require('fs');
 const fsExtra = require('fs-extra');
@@ -359,6 +361,7 @@ const SAMBA_SAVE_NAND_TRIES = 3;
 const SAMBA_SAVE_NAND_TIME = 10000;
 const DISCONNECT_CONTROLLER_TIMEOUT = 30000;
 const SW_FILE = path.join(__dirname, '../assets/sw.js');
+const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36";
 
 const DEFAULT_PROFILES_DICT = {
     "GuyStation Virtual Controller": {
